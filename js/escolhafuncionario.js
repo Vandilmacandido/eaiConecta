d5c814e3923f77a4ec828889fd553e470181661c
Carlos Negromonte <108841944+Negromontebjj@users.noreply.github.com>
2023-03-27 09:41:50 -0300

    atualização

--- js/escolhafuncionario.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.escolha');
-    var instances = M.FormSelect.init(elems, options);
-  });
-
-  $(document).ready(function(){
-    $('escolha').formSelect();
-  });
